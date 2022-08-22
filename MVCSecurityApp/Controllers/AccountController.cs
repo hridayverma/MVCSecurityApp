75116210f6f80cab264364e949a79ab0f43eea43
hridayverma <hrid.06@gmail.com>
2022-08-22 13:01:20 +0530

    Add project files.

---
+++ MVCSecurityApp/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSecurityApp.Models;
+using System.Web.Security;//namespace for Security related classes/interfaces
+
+namespace MVCSecurityApp.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+
+        CollegeDbEntities1 db = new CollegeDbEntities1();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //For login
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                Login luser=db.Logins
+                              .SingleOrDefault(u => u.Username == login.Username && u.Password == login.Password);
+                if (luser != null)
+                {
+                    //Generate Authentication Ticket after user Authentication from DB
+                    // FormsAuthentication.SetAuthCookie(login.Username, true);
+                    FormsAuthentication.SetAuthCookie(login.Username, false);
+                    //default URL/Action to redirect
+                    return RedirectToAction("Index", "Students");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Username/Password");
+                    return View(login);
+                }
+            }
+            return View(login);
+        }
+
+
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        //for signout
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            //return View("Login");
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ MVCSecurityApp/Controllers/FilterDemoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSecurityApp.Controllers
+{
+    [AllowAnonymous]
+    public class FilterDemoController : Controller
+    {
+        // GET: FilterDemo
+        public ActionResult Index()
+        {
+            /* try
+             {
+                 throw new Exception();
+             }
+             catch (Exception)
+             {
+
+                 ViewBag.Error = "Some error occured!!!"; 
+             }
+
+             */
+            throw new Exception();
+            return View();
+        }
+
+        //to check caching
+        [OutputCache(Duration =40,Location =System.Web.UI.OutputCacheLocation.Any)]
+        public ActionResult Index2()
+        {
+
+            ViewBag.CurrentTime = "Page Cached :" + DateTime.Now;
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
